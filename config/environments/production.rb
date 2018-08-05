fc0e889be56c67e194872fe197e3e0f5eab84272
SHIGAYUYA <shiga-yuya-yr@ynu.jp>
2018-08-06 05:11:06 +0900

    設定変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,11 +26,11 @@ Rails.application.configure do
   #config.assets.js_compressor = :uglifier
   #config.assets.js_compressor = Uglifier.new(harmony: true)
   #config.assets.js_compressor = Uglifier.new(:harmony => true)
-  #config.assets.js_compressor = Uglifier.new(harmony: true, compress: { unused: false })
-  config.assets.css_compressor = :sass
+  config.assets.js_compressor = Uglifier.new(harmony: true, compress: { unused: false })
+  config.assets.css_compressor = :scss
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
